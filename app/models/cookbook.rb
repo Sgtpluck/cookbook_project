86e0bd906c67175a6be01c95726307846ac8a75c
Davida Marion <davidamarion@gmail.com>
2013-12-03 14:42:29 -0800

    add associations

--- app/models/cookbook.rb
+++ app/models/cookbook.rb
@@ -1,3 +1,4 @@
 class Cookbook < ActiveRecord::Base
   belongs_to :user
+  has_many :recipes
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ActiveRecord::Base
+  belongs_to :cookbook
+  has_many :ingredients
+  has_many :gadgets
 end
