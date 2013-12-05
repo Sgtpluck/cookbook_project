057bb182cef18f01f3c43d4fad4bf62d7d8ae48d
Davida Marion <davidamarion@gmail.com>
2013-12-05 09:55:53 -0800

    trying to clear up problems

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,5 @@ class Recipe < ActiveRecord::Base
   has_many :gadgets_recipes
   has_many :ingredients, through: :ingredients_recipes
   has_many :gadgets, through: :gadgets_recipes
+  mount_uploader :display, DisplayUploader
 end
