9e8902b21caf80b70436b622bf82d2a7ac2c95a4
Davida Marion <davidamarion@gmail.com>
2013-12-05 16:45:04 -0800

    add search functionality

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,12 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients, through: :ingredients_recipes
   has_many :gadgets, through: :gadgets_recipes
   mount_uploader :display, DisplayUploader
+
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ? OR instructions LIKE ? or description LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%"])
+    else
+      find(:all)
+    end
+  end
 end
